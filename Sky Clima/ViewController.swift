e98e28400c5b410bd42c6cde6a517a774e7dea97
Iurii Kleimenov <yurakleimenovv@gmail.com>
2023-08-22 23:07:45 +0300

    Initial Commit

---
+++ Sky Clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sky Clima
+//
+//  Created by Юрий Клеймёнов on 22/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
